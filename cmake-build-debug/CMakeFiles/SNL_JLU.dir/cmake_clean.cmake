83cb3710ce318c83536c7e2cbf105bddcc7c43f6
zqxKarry <2908874296@qq.com>
2023-05-09 18:46:38 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/SNL_JLU.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SNL_JLU.dir/main.cpp.obj"
+  "SNL_JLU.exe"
+  "SNL_JLU.exe.manifest"
+  "SNL_JLU.pdb"
+  "libSNL_JLU.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SNL_JLU.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
